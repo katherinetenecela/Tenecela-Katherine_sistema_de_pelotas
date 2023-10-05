afc5f3a3ae9d51e04d50b425cf7beba0ca6f9f4f
KATHERINE YASBETH TENECELA MEJIA <katherine.tenecela@ucuenca.edu.ec>
2023-10-05 16:18:16 -0500

    pelota que rebota

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
+let mipelota;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  mipelota = new Pelota();
 }
 
 function draw() {
+  background(255);
   circle(mouseX, mouseY, 20);
+  mipelota.update(300);
+  mipelota.display();
 }
