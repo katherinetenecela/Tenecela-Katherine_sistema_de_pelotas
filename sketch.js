5157d77f038e80cc86621ccb8750212d5c45acfe
KATHERINE YASBETH TENECELA MEJIA <katherine.tenecela@ucuenca.edu.ec>
2023-10-20 22:16:20 -0500

    colores

--- sketch.js
+++ sketch.js
@@ -1,17 +1,19 @@
 pelotas = [];
 const NP = 200;
+let fondoColor;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   for (let i = 0; i < NP; i++) {
     let pelota = new Pelota();
     pelotas.push(pelota);
+    fondoColor = color(random(255), random(255), random(255));
   }
   print(pelotas);
 }
 
 function draw() {
-  background(163, 99, 252);
+  background(fondoColor);
   for (let i = 0; i < NP; i++) {
     pelotas[i].update(windowHeight);
     pelotas[i].display();
